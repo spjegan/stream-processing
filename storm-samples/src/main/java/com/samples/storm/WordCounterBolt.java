87c63e487c7d9994da15823449bc6acc9c73a88b
PonramaJegan <Ponrama.Jegan@bestbuy.com>
2015-04-06 20:14:06 -0500

    Initial Commit

---
+++ storm-samples/src/main/java/com/samples/storm/WordCounterBolt.java
@@ -0,0 +1,64 @@
+package com.samples.storm;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Tuple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Jegan on 4/6/2015.
+ */
+public class WordCounterBolt implements IRichBolt {
+
+    private Integer id;
+    private String name;
+    private Map<String, Integer> counters;
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        this.counters = new HashMap<String, Integer>();
+        this.collector = collector;
+        this.name = context.getThisComponentId();
+        this.id = context.getThisTaskId();
+    }
+
+    @Override
+    public void execute(Tuple input) {
+        String str = input.getString(0);
+        /**
+         * If the word dosn't exist in the map we will create
+         * this, if not We will add 1
+         */
+        if (!counters.containsKey(str)) {
+            counters.put(str, 1);
+        } else {
+            Integer c = counters.get(str) + 1;
+            counters.put(str, c);
+        }
+        //Set the tuple as Acknowledge
+        collector.ack(input);
+    }
+
+    @Override
+    public void cleanup() {
+        System.out.println("-- Word Counter [" + name + "-" + id + "] --");
+        for (Map.Entry<String, Integer> entry : counters.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+}
